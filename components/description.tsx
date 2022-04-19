156fd9bc41e2c6f2cfd0b725b35198fbe297f1e9
leogadil <leogadilvlog@gmail.com>
2022-04-20 02:59:28 +0800

    update classnames

--- components/description.tsx
+++ components/description.tsx
@@ -7,12 +7,12 @@ type IProps = {
 
 const Description : React.FC<IProps> = ({title, description}) => {
     return (
-        <div className="grid sm:grid-cols-3 gap-2 grid-rows-2s mb-10 px-10">
+        <div className="grid sm:grid-cols-3 gap-2 grid-rows-2s mb-10 px-10 w-full">
 				<div className="text-left">
 					<span className="font-black text-lg sm:text-xl font-mplus dark:text-main-accent text-accent-2 tracking-widest">
 						{title}
 					</span>
-					<div className="w-15 sm:w-32 h-1 rounded bg-accent-2 dark:bg-main-accent">
+					<div className="w-10 sm:w-32 h-1 rounded bg-accent-2 dark:bg-main-accent">
 					</div>
 				</div>
 				<div className="col-span-2 break-words">
