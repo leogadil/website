dc515d3bf6dda927420912456c6672cbdd53989c
leogadil <leogadilvlog@gmail.com>
2022-04-21 02:46:54 +0800

    disabled scrolling when changing page

--- components/buttonlink.tsx
+++ components/buttonlink.tsx
@@ -14,7 +14,7 @@ type IButtonLinkProps = {
 const ButtonLink : React.FC<IButtonLinkProps> = ({ text, url, delay }) => {
     return (
         <AnimateSection delay={delay}>
-            <Link href={url}>
+            <Link href={url} scroll={false}>
                 <button className={"bg-accent-2 dark:bg-main-accent mb-5 p-2 px-3 text-th-light rounded flex flex-row justify-center items-center hover:bg-accent-2-light hover:dark:bg-main-accent-hover transition-colors"}>
                     <span>{text}</span>
                     <FaAngleRight className="ml-1 mt-[3.5px] text-th-light" />
