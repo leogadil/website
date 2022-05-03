e9ae069ee0087e1876cb14eb6fc66f74bce82caf
leogadil <leoagadilvlog@gmail.com>
2022-05-03 15:40:25 +0800

    added link to source code of the website

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import { useTheme } from 'next-themes'
 
 import {IoIosMoon, IoMdSunny} from 'react-icons/io';
+import { DiGithubBadge } from 'react-icons/di';
 import {useRouter} from 'next/router';
 import Link from 'next/link'
 
@@ -26,18 +27,30 @@ const Header : React.FC = () => {
 				</div>
 				<div className="flex flex-row items-center">
 					<ul className="flex mr-4">
-						<li className="flex-1 mr-3 cursor-pointer">
+						<li className="flex-1 mr-3 cursor-pointer text-lg">
 							<Link href="/projects" scroll={false}>
 								<button className="hover:dark:text-main-accent hover:text-accent-2 transition-colors">Projects</button>
 							</Link>
 							<div className={"w-full h-1 rounded bg-transparent dark:bg-transparent transition-colors " + (Sroute.toLowerCase().indexOf('/projects') != -1 ? 'bg-accent-2 dark:bg-main-accent' : '')}>
 							</div>
 						</li>
-						<li className="flex-1 cursor-pointer">
+						<li className="flex-1 mr-3 cursor-pointer text-lg">
 							<Link href="/blogs" scroll={false}>
 							<button className="hover:dark:text-main-accent hover:text-accent-2 transition-colors">Blogs</button>
 							</Link>
 							<div className={"w-full h-1 rounded bg-transparent dark:bg-transparent transition-colors " + (Sroute.toLowerCase().indexOf('/blogs') != -1 ? 'bg-accent-2 dark:bg-main-accent' : '')}>
+							</div>
+						</li>
+						<li className="flex-1 cursor-pointer text-lg">
+							<Link passHref href="/" scroll={false}>
+								<a href="https://github.com/leogadil/website" target="_blank" rel="noopener noreferrer">
+									<button className="hover:dark:text-main-accent hover:text-accent-2 transition-colors flex flex-row items-center ">
+										<DiGithubBadge className='' />
+										Source
+									</button>
+								</a>
+							</Link>
+							<div className={"w-full h-1 rounded bg-transparent dark:bg-transparent transition-colors "}>
 							</div>
 						</li>
 					</ul>
