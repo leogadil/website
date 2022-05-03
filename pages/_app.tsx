f0bed4bd1b504c195851b0e462598dde717c5316
leogadil <leoagadilvlog@gmail.com>
2022-05-03 16:43:29 +0800

    added progress bar when loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,35 @@ import { ThemeProvider } from 'next-themes'
 import { AnimatePresence } from 'framer-motion'
 import {useRouter} from 'next/router';
 
+import Router from 'next/router'
+import nProgress from 'nprogress'
 import Header from '../components/header'
+
 
 
 function MyApp({ Component, pageProps }: AppProps) {
 
 	const router = useRouter()
 
+	useEffect(() => {
+		const handlleRouteStart = () => nProgress.start()
+		const handleRouteEnd = () => nProgress.done()
+		
+		Router.events.on('routeChangeStart', handlleRouteStart)
+		Router.events.on('routeChangeComplete', handleRouteEnd)
+		Router.events.on('routeChangeError', handleRouteEnd)
+
+		return () => {
+			Router.events.off('routeChangeStart', handlleRouteStart)
+			Router.events.off('routeChangeComplete', handleRouteEnd)
+			Router.events.off('routeChangeError', handleRouteEnd)
+		}
+	})
+
 	return (
 		<ThemeProvider attribute='class' enableColorScheme>
 			<div className="flex flex-col">
 				<Header />
-				{/* <NotificationContainer /> */}
 				<div className="w-full max-w-lg mx-auto mt-32 flex-1 flex flex-col items-center justify-center">
 					<AnimatePresence exitBeforeEnter>
 						<Component {...pageProps} key={router.pathname}/>
