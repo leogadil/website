62d0770570de5f21fb6c6d1422eba3237b6d13dc
leogadil <leogadil04@gmail.com>
2023-07-25 23:41:26 +0800

    updated cv

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -4,7 +4,7 @@ const resume = () => {
   return (
     <div className="w-screen h-screen">
       <HeadMeta title="Resume" />
-      <iframe src="/cv2022.pdf" className="w-full h-full" />
+      <iframe src="/cv2023.pdf" className="w-full h-full" />
     </div>
   )
 }
