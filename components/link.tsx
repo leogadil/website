b34a3f099c908939b9ea57865151ec6e8aecf894
leogadil <leogadilvlog@gmail.com>
2023-10-16 20:10:24 +0800

    added url checker to gethostname

--- components/link.tsx
+++ components/link.tsx
@@ -7,6 +7,17 @@ import { CgLink } from 'react-icons/cg'
 const Link: React.FC<ILinkProps> = ({ link, text, className, showLinkIcon }) => {
 
     const getHostname = (url: string) => {
+        //check if url is valid or not
+        const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i') // fragment locator
+        if(!pattern.test(url)) {
+            return url
+        }
+
         try {
             const hostname = new URL(url).hostname
             return hostname
